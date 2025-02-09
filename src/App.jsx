fadb3936ee1957f314322e11ed13f77a1000f020
Andrii Gnedash <andrii.gnedash@ringcentral.com>
2025-02-09 16:53:43 +0100

    hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,63 @@
-import userData from "./userData.json";
-import friends from "./friends.json";
-import transactions from "./transactions.json";
-import Profile from "./components/Profile/Profile";
-import FriendList from "./components/FriendList/FriendList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import "./App.css"; 
+import { useState, useEffect } from "react";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
+import "./App.css";
 
 const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem("feedback")) || {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      }
+    );
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevState) => ({
+      ...prevState,
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
   return (
     <div className="appContainer">
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
       />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          total={totalFeedback}
+          positive={positiveFeedback}
+        />
+      ) : (
+        <Notification message="No feedback yet. Be the first to leave a review!" />
+      )}
     </div>
   );
 };
